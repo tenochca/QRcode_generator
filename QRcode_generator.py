27b82632d34302cda9517080a183022fba1e4023
Diego Izaguirre <tenochca.di@outlook.com>
2023-06-29 11:53:44 -0500

    utlized methods from the imported QRCode class to customize size ,shape ,pattern, and color.

--- QRcode_generator.py
+++ QRcode_generator.py
@@ -2,6 +2,11 @@ import qrcode
 
 data = 'Hello!'
 
-img = qrcode.make(data)
+qr = qrcode.QRCode(version = 2, box_size = 10, border = 5)
 
-img.save('C:/Users/tenoc/Pictures/Camera Roll/myqrcode.png')
+qr.add_data(data)
+
+qr.make(fit = True)
+img = qr.make_image(fill_color = 'red', back_color = 'green')
+
+img.save('C:/Users/tenoc/Pictures/Camera Roll/myqrcode2.png')
