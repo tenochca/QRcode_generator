80a184a71a6507863b0e3882413c269e7851aba0
Diego Izaguirre <tenochca.di@outlook.com>
2023-07-01 19:14:42 -0500

    added comments and generated qrcode example

--- QRcode_generator.py
+++ QRcode_generator.py
@@ -1,12 +1,15 @@
 import qrcode
 
-data = 'Hello!'
+data = 'https://www.google.com/logos/2010/pacman10-i.html' #data to be encoded in the qrcode
 
-qr = qrcode.QRCode(version = 2, box_size = 10, border = 5)
+qr = qrcode.QRCode(version = 2, box_size = 10, border = 5) #creating a QRCode object and using qrcode method to modify it.
 
-qr.add_data(data)
+qr.add_data(data) #adding the specified data using add_data function
 
-qr.make(fit = True)
-img = qr.make_image(fill_color = 'red', back_color = 'green')
+qr.make(fit = True) #compiles the data into an array. 
+#setting fit to true automaticly sized the qr code accordingly
 
-img.save('C:/Users/tenoc/Pictures/Camera Roll/myqrcode2.png')
+img = qr.make_image(fill_color = 'green', back_color = 'white') #make_image functin specifies color
+
+img.save('C:/Users/tenoc/Pictures/Camera Roll/myqrcode.png') #save the image to designated file path
+
